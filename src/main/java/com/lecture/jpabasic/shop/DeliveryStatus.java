219e73aef72318f8c821526c955e6d0af134184e
mybloom <baejungeun11@gmail.com>
2022-05-05 00:06:28 +0900

    chap7: 실전예제3 다양한 연관관계 매핑

---
+++ src/main/java/com/lecture/jpabasic/shop/DeliveryStatus.java
@@ -0,0 +1,4 @@
+package com.lecture.jpabasic.shop;
+
+public enum DeliveryStatus {
+}
--- src/main/java/com/lecture/jpabasic/shop/Item.java
+++ src/main/java/com/lecture/jpabasic/shop/Item.java
@@ -1,9 +1,12 @@
 package com.lecture.jpabasic.shop;
 
+import java.util.ArrayList;
+import java.util.List;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.persistence.ManyToMany;
 
 @Entity
 public class Item {
@@ -16,4 +19,8 @@ public class Item {
 	private String name;
 	private int price;
 	private int stockQuantity;
+
+	@ManyToMany(mappedBy = "items")
+	private List<Category> categories = new ArrayList<>();
+
 }
