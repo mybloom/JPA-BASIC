ff5a439f44b6efee0e527223019427b6753d340a
mybloom <baejungeun11@gmail.com>
2022-05-04 21:22:35 +0900

    chap6: 양뱡향 매핑시 가장 많이 하는 실수 - 연관관계 주인에 값을 입력하지 않음

--- src/main/java/com/lecture/jpabasic/hellojpa/JpaMain.java
+++ src/main/java/com/lecture/jpabasic/hellojpa/JpaMain.java
@@ -17,34 +17,17 @@ public class JpaMain {
 		transaction.begin();
 
 		try {
-			Team team = new Team();
-			team.setName("TeamA");
-			entityManager.persist(team);
-
 			Member member1 = new Member();
 			member1.setUserName("member1");
-			member1.setTeam(team);
 			entityManager.persist(member1);
 
-			Member member2 = new Member();
-			member2.setUserName("member2");
-			member2.setTeam(team);
-			entityManager.persist(member2);
-
-			Member member3 = new Member();
-			member3.setUserName("member3");
-			member3.setTeam(team);
-			entityManager.persist(member3);
+			Team team = new Team();
+			team.setName("TeamA");
+			team.getMembers().add(member1); //member에 team이 저장되지 않는다.
+			entityManager.persist(team);
 
 			entityManager.flush();
 			entityManager.clear();
-
-			Member findMember = entityManager.find(Member.class, member3.getId());
-			List<Member> members = findMember.getTeam().getMembers();
-
-			for (Member member : members) {
-				System.out.println("member = " + member.getUserName());
-			}
 
 			transaction.commit();
 		} catch (Exception e) {
