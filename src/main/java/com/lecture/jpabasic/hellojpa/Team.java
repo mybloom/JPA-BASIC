c9af13098fe4f305311a5de9264645f60a305ede
mybloom <baejungeun11@gmail.com>
2022-05-04 23:36:52 +0900

    chap7: 일대일 주 테이블/대상 테이블에 외래키 장/단점 정리

--- src/main/java/com/lecture/jpabasic/hellojpa/Team.java
+++ src/main/java/com/lecture/jpabasic/hellojpa/Team.java
@@ -1,13 +1,9 @@
 package com.lecture.jpabasic.hellojpa;
 
-import java.util.ArrayList;
-import java.util.List;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
-import javax.persistence.JoinColumn;
-import javax.persistence.OneToMany;
 import lombok.Getter;
 import lombok.Setter;
 
